96d4a64a2328befab0cb677b1c61f669eed720ea
Keith Austin Jr <keiaus@Keiths-MBP.localdomain>
2023-03-14 15:19:29 -0400

    updates

--- holly/main.py
+++ holly/main.py
@@ -2,20 +2,12 @@ from tkinter import *
 
 root = Tk()
 root.title('Home')
-root.wm_attributes('-fullscreen', 1)
+root.state('zoomed')
 
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 
-def show_frame(frame):
-    frame.tkraise()
-
 main_page = Frame(root)
-
-show_frame(main_page)
-
-for frame in (main_page):
-    frame.grid(row=0, column=0, sticky='nsew')
 
 header = Label(main_page, text='Welcome to Holly', font='times 50 bold', bg='Skyblue', anchor=N, pady=50)
 header.pack(fill='BOTH')
