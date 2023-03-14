2504b610b73082caa3aa01fab938825b2b4a3ef5
keiaus <k.aus2029@gmail.com>
2023-03-14 16:57:16 -0400

    Fixed the frame

--- holly/main.py
+++ holly/main.py
@@ -10,6 +10,6 @@ root.columnconfigure(0, weight=1)
 main_page = Frame(root)
 
 header = Label(main_page, text='Welcome to Holly', font='times 50 bold', bg='Skyblue', anchor=N, pady=50)
-header.pack(fill='BOTH')
+header.pack(fill='both')
 
 root.mainloop()
