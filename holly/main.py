e6a3f50aba4e4ca531b501a30c5ff41b479c2cd5
Keith Austin Jr <keiaus@Keiths-MBP.localdomain>
2023-03-15 10:53:45 -0400

    Classes in Python :)

--- holly/main.py
+++ holly/main.py
@@ -1,15 +1,18 @@
 from tkinter import *
 
+class GUI:
+    def __init__(self, root):
+        root.title('Home')
+        root.state('zoomed')
+
+        root.rowconfigure(0, weight=1)
+        root.columnconfigure(0, weight=1)
+
+        main_page = Frame(root)
+
+        header = Label(main_page, text='Welcome to Holly', font='times 50 bold', bg='Skyblue', anchor=N, pady=50)
+        header.pack(fill='both')
+    
 root = Tk()
-root.title('Home')
-root.state('zoomed')
-
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
-
-main_page = Frame(root)
-
-header = Label(main_page, text='Welcome to Holly', font='times 50 bold', bg='Skyblue', anchor=N, pady=50)
-header.pack(fill='both')
-
+GUI(root)
 root.mainloop()
