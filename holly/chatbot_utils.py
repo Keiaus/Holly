6f8a77e7538cc3175c595e48d73644bfd6470b54
keiaus <k.aus2029@gmail.com>
2023-03-20 11:26:15 -0400

    updates

--- holly/chatbot_utils.py
+++ holly/chatbot_utils.py
@@ -1,10 +1,8 @@
-import tkinter as tk
 from __future__ import absolute_import
-from __future__ import division 
+from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-
-import torch 
+import torch
 from torch.jit import script, trace
 import torch.nn as nn
 from torch import optim
