63c6318d32260d81aab09f603bf663bb06cd4040
Keith Austin Jr <keiaus@Keiths-MacBook-Pro.local>
2023-03-20 13:33:34 -0400

    data file progress

--- holly/data/data.py
+++ holly/data/data.py
@@ -22,3 +22,14 @@ import json
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
+
+corpus_name = "movie-corpus"
+corpus = os.path.join("data", corpus_name)
+
+def printLines(file, n=10):
+    with open(file, 'rb') as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
+
+printLines(os.path.join(corpus, "utterances.jsonl"))
